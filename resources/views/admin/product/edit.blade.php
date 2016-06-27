ef221073f5993d50e91efdd502c9382ba344a1db
ipoplary <poplary_y@163.com>
2016-06-27 08:08:33 +0800

    create product

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -8,87 +8,92 @@
 
     <div class="row" id="app">
         <div class="col-md-12">
-            <form class="">
-
-                <div class="control-group form-horizontal col-md-4">
-                    <label class="control-label" for="input01">Text input</label>
-                    <div class="controls">
-                        <input type="text" placeholder="placeholder" class="form-control">
+            <form class="form-horizontal">
+                <fieldset>
+                    <div>
+                        <legend class="">添加商品</legend>
                     </div>
-                </div>
 
-                <div class="control-group">
-                    <label class="control-label" for="input01">Text input</label>
-                    <div class="controls">
-                        <input type="text" placeholder="placeholder" class="input-xlarge">
-
-                    </div>
-                </div>
-
-                <div class="control-group">
-                    <label class="control-label">Select - Basic</label>
-                    <div class="controls">
-                        <select class="input-xlarge">
-                            <option>Enter</option>
-                            <option>Your</option>
-                            <option>Options</option>
-                            <option>Here!</option>
-                        </select>
-                    </div>
-                </div>
-
-                <div class="control-group">
-                    <label class="control-label" for="input01">Text input</label>
-                    <div class="controls">
-                        <input type="text" placeholder="placeholder" class="input-xlarge">
-                    </div>
-                </div>
-
-                <div class="control-group">
-                    <label class="control-label" for="input01">Text input</label>
-                    <div class="controls">
-                        <input type="text" placeholder="placeholder" class="input-xlarge">
-                    </div>
-                </div>
-
-                <div class="control-group">
-                    <label class="control-label" for="input01">Text input</label>
-                    <div class="controls">
-                        <input type="text" placeholder="placeholder" class="input-xlarge">
-                        <p class="help-block">Supporting help text</p>
-                    </div>
-                </div>
-
-                <div class="control-group">
-                    <label class="control-label" for="input01">Text input</label>
-                    <div class="controls">
-                        <input type="text" placeholder="placeholder" class="input-xlarge">
-                        <p class="help-block">Supporting help text</p>
-                    </div>
-                </div>
-
-                <div class="control-group">
-                    <label class="control-label">Textarea</label>
-                    <div class="controls">
-                        <div class="textarea">
-                            <textarea type="" class=""> </textarea>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2">Text input</label>
+                        <div class="controls col-sm-3">
+                            <input type="text" placeholder="placeholder" class="form-control">
                         </div>
                     </div>
-                </div>
 
-                <div class="control-group">
-                    <label class="control-label" for="input01">Text input</label>
-                    <div class="controls">
-                        <input type="text" placeholder="placeholder" class="input-xlarge">
-                    </div>
-                </div>
+                    <div class="form-group">
+                        <label class="control-label col-sm-2" for="input01">Text input</label>
+                        <div class="controls col-sm-3">
+                            <input type="text" placeholder="placeholder" class="form-control">
 
-                <div class="control-group">
-                    <label class="control-label" for="input01">Text input</label>
-                    <div class="controls">
-                        <input type="text" placeholder="placeholder" class="input-xlarge">
+                        </div>
                     </div>
-                </div>
+
+                    <div class="control-group">
+                        <label class="control-label">Select - Basic</label>
+                        <div class="controls">
+                            <select class="input-xlarge">
+                                <option>Enter</option>
+                                <option>Your</option>
+                                <option>Options</option>
+                                <option>Here!</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="control-group">
+                        <label class="control-label" for="input01">Text input</label>
+                        <div class="controls">
+                            <input type="text" placeholder="placeholder" class="input-xlarge">
+                        </div>
+                    </div>
+
+                    <div class="control-group">
+                        <label class="control-label" for="input01">Text input</label>
+                        <div class="controls">
+                            <input type="text" placeholder="placeholder" class="input-xlarge">
+                        </div>
+                    </div>
+
+                    <div class="control-group">
+                        <label class="control-label" for="input01">Text input</label>
+                        <div class="controls">
+                            <input type="text" placeholder="placeholder" class="input-xlarge">
+                            <p class="help-block">Supporting help text</p>
+                        </div>
+                    </div>
+
+                    <div class="control-group">
+                        <label class="control-label" for="input01">Text input</label>
+                        <div class="controls">
+                            <input type="text" placeholder="placeholder" class="input-xlarge">
+                            <p class="help-block">Supporting help text</p>
+                        </div>
+                    </div>
+
+                    <div class="control-group">
+                        <label class="control-label">Textarea</label>
+                        <div class="controls">
+                            <div class="textarea">
+                                <textarea type="" class=""> </textarea>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="control-group">
+                        <label class="control-label" for="input01">Text input</label>
+                        <div class="controls">
+                            <input type="text" placeholder="placeholder" class="input-xlarge">
+                        </div>
+                    </div>
+
+                    <div class="control-group">
+                        <label class="control-label" for="input01">Text input</label>
+                        <div class="controls">
+                            <input type="text" placeholder="placeholder" class="input-xlarge">
+                        </div>
+                    </div>
+                </fieldset>
             </form>
         </div>
     </div>
